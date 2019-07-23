e37aa2088741270c0012ea26dbbeb6cda33b7e49
amita-shukla <amitashukla0906@gmail.com>
2019-07-23 12:48:50 +0530

    create simple routing, posting to server everytime the url changes

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,9 @@ import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 
 function App() {
+  // get route from window and display page accprdingly.
+  // but the downside is it's posting back to server
+  // and we lose client-side state.
   const route = window.location.pathname;
   if (route === "/about") return <AboutPage />;
   return <HomePage />;
