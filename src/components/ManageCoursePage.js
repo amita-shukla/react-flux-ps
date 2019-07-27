76c2f16563c77ea57e5f3a28f08f3ce02ddba37a
amita-shukla <amitashukla0906@gmail.com>
2019-07-28 04:34:11 +0530

    create form and add onChange handler for a an input

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,12 +1,24 @@
-import React from "react";
-import { Prompt } from "react-router-dom";
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
 
 const ManageCoursePage = props => {
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  function handleTitleChange(event) {
+    const updatedCourse = { ...course, title: event.target.value };
+    setCourse(updatedCourse);
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      <Prompt when={true} message="Are you sure you want to leave?" />
-      {props.match.params.slug}
+      <CourseForm course={course} onTitleChange={handleTitleChange} />
     </>
   );
 };
